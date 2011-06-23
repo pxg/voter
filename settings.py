63ab7bc11986a73391d43d52c87e0cfbdcc46dec
Pete X. Graham <pete.graham@piratalondon.com>
2011-06-23 15:24:25 +0100

    modified files

--- settings.py
+++ settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'polls',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
